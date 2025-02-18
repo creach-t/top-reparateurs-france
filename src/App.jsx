30d27cc01e9d902a382f013404744ffd3fb26eed
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-02-18 12:58:45 +0100

    feat: ajout des fichiers principaux de l'application

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RepairShops from './components/RepairShops';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <RepairShops />
+    </div>
+  );
+}
+
+export default App;
